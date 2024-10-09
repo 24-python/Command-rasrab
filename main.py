bf8a30f00be05fbb3727efea7ded99bcae8e9eee
24-python <24_python@mail.ru>
2024-10-09 19:56:46 +0300

    1

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import sqlite3
 
 conn = sqlite3.connect('exemple.db')
 cur = conn.cursor()
-cur.execute('''
+# cur.execute('''
+#
+# CREATE TABLE IF NOT EXISTS users (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     age INTEGER NOT NULL
+# );
+# 
+# ''')
+# conn.commit()
+# cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Алексей', 25))
+# cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Игорь', 36))
+# conn.commit()
 
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    age INTEGER NOT NULL
-);
+cur.execute("SELECT * FROM users")
+rows = cur.fetchall()
 
-''')
-conn.commit()
-cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Алексей', 25))
-cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Игорь', 36))
-conn.commit()
+for row in rows:
+    print(row)
 
 conn.close()
