08018e2a7be28ad9107946a0d7046374e76f9acc
24-python <24_python@mail.ru>
2024-10-09 19:23:28 +0300

    create DB

---
+++ main.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('exemple.db')
+cur = conn.cursor()
+cur.execute('''
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL
+);
+
+''')
+conn.commit()
+cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Алексей', 25))
+cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Игорь', 36))
+conn.commit()
+
+conn.close()
